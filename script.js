621b17412a4767a9d04db42d53023968088be028
Stiwari0321 <133880209+Stiwari0321@users.noreply.github.com>
2023-07-06 12:22:04 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-var box = document.getElementById("colorBox");
-var interval = setInterval(function() {
-box.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
-}, 2000);
